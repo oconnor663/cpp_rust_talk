ffc4acb0101e4519b2e5fceff4654053a8800275
Jack O'Connor <oconnor663@gmail.com>
2021-05-09 22:49:57 -0400

    use the same string

--- 1.a.lifetimes/string_view_rs/src/main.rs
+++ 1.a.lifetimes/string_view_rs/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let my_string: String = "hello".to_string();
-    let my_string_view: &str = (my_string + " world").as_str();
+    let my_string: String = "abcdefghijklmnopqrstuvwxy".to_string();
+    let my_string_view: &str = (my_string + "z").as_str();
     dbg!(my_string_view);
 }
