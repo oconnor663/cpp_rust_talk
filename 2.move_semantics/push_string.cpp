4b795c2603f529bd22c277cba3ba00877b95d1e8
Jack O'Connor <oconnor663@gmail.com>
2021-05-11 01:30:47 -0400

    use a longer string for push_string

--- 2.move_semantics/push_string.cpp
+++ 2.move_semantics/push_string.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main() {
-  std::string my_string = "hello world";
+  std::string my_string = "abcdefghijklmnopqrstuvwxyz";
   std::vector<std::string> my_vector;
   my_vector.push_back(my_string);
   my_vector.push_back(std::move(my_string));
--- 2.move_semantics/push_string_rs/src/main.rs
+++ 2.move_semantics/push_string_rs/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let my_string = "hello world".to_string();
+    let my_string = "abcdefghijklmnopqrstuvwxyz".to_string();
     let mut my_vector = Vec::new();
     my_vector.push(my_string.clone());
     my_vector.push(my_string);
