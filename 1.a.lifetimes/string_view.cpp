b78f3f04ef25f3714479a542b7884db821a15f95
Jack O'Connor <jack.oconnor@zoom.us>
2021-05-19 21:00:50 -0400

    format the string_view example

--- 1.a.lifetimes/string_view.cpp
+++ 1.a.lifetimes/string_view.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main() {
-  string my_string = "abcdefghijklmnopqrstuvwxy";
-  string_view my_string_view = my_string + "z";
+  string my_string =
+      "abcdefghijklmnopqrstuvwxy";
+  string_view my_string_view =
+      my_string + "z";
   cout << my_string_view;
 }
--- 1.a.lifetimes/string_view_rs/src/main.rs
+++ 1.a.lifetimes/string_view_rs/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let my_string: String =
         "abcdefghijklmnopqrstuvwxy".to_string();
-    let my_string_view: &str = (my_string + "z").as_str();
+    let my_string_view: &str =
+        (my_string + "z").as_str();
     dbg!(my_string_view);
 }
